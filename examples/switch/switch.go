a937375ef257d0aa2c9bc8e79ce60303be1a7deb
Fox Lee <1159542665@qq.com>
2021-12-10 00:27:43 +0800

    effective go switch

--- examples/switch/switch.go
+++ examples/switch/switch.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	breakTest()
+	compare:= Compare([]byte("aasd"), []byte("aacd"))
+	println(compare)
+	//breakTest()
 	//chooseFruit("apple")
 	//chooseFruit("aa")
+}
+// Compare 比较两个字节型切片，返回一个整数
+// 按字典顺序.
+// 如果a == b，结果为0；如果a < b，结果为-1；如果a > b，结果为+1
+func Compare(a, b []byte) int {
+	for i := 0; i < len(a) && i < len(b); i++ {
+		switch {
+		case a[i] > b[i]:
+			return 1
+		case a[i] < b[i]:
+			return -1
+		}
+	}
+	switch {
+	case len(a) > len(b):
+		return 1
+	case len(a) < len(b):
+		return -1
+	}
+	return 0
 }
 
 func breakTest() {
@@ -17,7 +41,7 @@ func breakTest() {
 				fmt.Printf("inner loop is %d \n", j)
 				if j == 0 {
 					fmt.Printf("break Loop \n")
-					break Loop
+					continue Loop
 				}
 				if j == 1 {
 					fmt.Printf("break Out \n")
