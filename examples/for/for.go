ab5b9ea329fb51f6abc52607c720c82d73192366
Fox Lee <1159542665@qq.com>
2021-12-10 00:13:05 +0800

    effective go exercise

--- examples/for/for.go
+++ examples/for/for.go
@@ -6,7 +6,26 @@ import (
 )
 
 func main() {
-	s:=[]int{1,2,3,4,5,6,7,8,9}
+	//efFor()
+}
+
+// Reverse a
+func Reverse() {
+	a := []int8{1, 2, 3, 4, 5, 6}
+	// i, j = i++, j-- 非法，++ 是语句而非表达式
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+}
+
+func efFor() {
+	for pos, char := range "日本\x80語" { // \x80 在 UTF-8 编码中是一个非法字符
+		fmt.Printf("character %#U starts at byte position %d\n", char, pos)
+	}
+}
+
+func tryFor() {
+	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	for i, v := range s {
 		fmt.Printf("index is %d, value is %d \n", i, v)
 	}
--- examples/switch/switch.go
+++ examples/switch/switch.go
@@ -3,8 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	chooseFruit("apple")
-	chooseFruit("aa")
+	breakTest()
+	//chooseFruit("apple")
+	//chooseFruit("aa")
+}
+
+func breakTest() {
+	Out:
+	for i := 0; i < 10; i++ {
+		fmt.Printf("current loop is %d \n", i)
+		Loop:
+			for j := 0; j < 5; j++ {
+				fmt.Printf("inner loop is %d \n", j)
+				if j == 0 {
+					fmt.Printf("break Loop \n")
+					break Loop
+				}
+				if j == 1 {
+					fmt.Printf("break Out \n")
+					break Out
+				}
+			}
+	}
+	fmt.Printf("end\n")
 }
 
 func chooseFruit(s string) {
