656e461103ec29b1290c13e4d7c00167bd48eff8
Fox Lee <1159542665@qq.com>
2021-12-10 00:54:57 +0800

    effective go type switch

--- examples/switch/switch.go
+++ examples/switch/switch.go
@@ -5,12 +5,47 @@ import (
 )
 
 func main() {
-	compare:= Compare([]byte("aasd"), []byte("aacd"))
-	println(compare)
+	// 断言
+	//t := true
+	var t *string
+	fmt.Println(t)
+	//var t interface{}
+	switchType(t)
+	//var i interface{}= 1
+	//t1:= i.(int)
+	//fmt.Println(t1)
+	//
+	//t2, ok:= i.(int)
+	//if ok {
+	//	fmt.Println("this is int ", t2)
+	//}
+
+	//compare:= Compare([]byte("aasd"), []byte("aacd"))
+	//println(compare)
 	//breakTest()
 	//chooseFruit("apple")
 	//chooseFruit("aa")
 }
+
+func switchType(i interface{}) {
+	switch t := i.(type) {
+	default:
+		fmt.Printf("unexpected type %T\n", t) // %T 打印任何类型的 t
+	case nil:
+		fmt.Printf("nil %t\n", t) // t 是 nil
+	case bool:
+		fmt.Printf("boolean %t\n", t) // t 是 bool 类型
+	case int:
+		fmt.Printf("integer %d\n", t) // t 是 int 类型
+	case *bool:
+		fmt.Printf("pointer to boolean %t\n", *t) // t 是 *bool 类型
+	case *int:
+		fmt.Printf("pointer to integer %d\n", *t) // t 是 *int 类型
+	case *string:
+		fmt.Printf("pointer to string \n") // t 是 *string 类型
+	}
+}
+
 // Compare 比较两个字节型切片，返回一个整数
 // 按字典顺序.
 // 如果a == b，结果为0；如果a < b，结果为-1；如果a > b，结果为+1
@@ -33,21 +68,21 @@ func Compare(a, b []byte) int {
 }
 
 func breakTest() {
-	Out:
+Out:
 	for i := 0; i < 10; i++ {
 		fmt.Printf("current loop is %d \n", i)
-		Loop:
-			for j := 0; j < 5; j++ {
-				fmt.Printf("inner loop is %d \n", j)
-				if j == 0 {
-					fmt.Printf("break Loop \n")
-					continue Loop
-				}
-				if j == 1 {
-					fmt.Printf("break Out \n")
-					break Out
-				}
+	Loop:
+		for j := 0; j < 5; j++ {
+			fmt.Printf("inner loop is %d \n", j)
+			if j == 0 {
+				fmt.Printf("break Loop \n")
+				continue Loop
 			}
+			if j == 1 {
+				fmt.Printf("break Out \n")
+				break Out
+			}
+		}
 	}
 	fmt.Printf("end\n")
 }
